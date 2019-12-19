b93a36a5c14331e9c2315003a45eccffab95eac0
Hiroshi Urabe <mail@torounit.com>
2019-12-20 02:30:59 +0900

    fix permalink

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   templates: {
     WordPressCategory: '/category/:slug', // adds route for "category" post type (Optional)
-    WordPressPost: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
+    WordPressPost: '/:year/:month/:day/:id', //adds route for "post" post type (Optional)
     WordPressPostTag: '/tag/:slug', // adds route for "post_tag" post type (Optional)
     WordPressPage: [
       {
