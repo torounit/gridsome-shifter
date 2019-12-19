ec2f131a6c0a971e01ead5a7fb96c5cab1c26ec3
Hiroshi Urabe <mail@torounit.com>
2019-12-20 01:30:10 +0900

    support wordpress pages

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,14 +5,15 @@ module.exports = {
   templates: {
     WordPressCategory: '/category/:slug', // adds route for "category" post type (Optional)
     WordPressPost: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
-    WordPressPostTag: '/tag/:slug' // adds route for "post_tag" post type (Optional)
+    WordPressPostTag: '/tag/:slug', // adds route for "post_tag" post type (Optional)
+    WordPressPage: '/:slug' // adds route for "post_tag" post type (Optional)
   },
 
   plugins: [
     {
       use: '@gridsome/source-wordpress',
       options: {
-        baseUrl: process.env.CONTAINER_URL.replace('https://', ''),
+        baseUrl: process.env.CONTAINER_URL,
         typeName: 'WordPress', // GraphQL schema name (Optional)
       }
     }
---
+++ src/templates/WordPressPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <Layout>
+    <h1 v-html="$page.wordPressPage.title"/>
+    <img
+      v-if="$page.wordPressPage.featuredMedia"
+      :src="$page.wordPressPage.featuredMedia.sourceUrl"
+      :width="$page.wordPressPage.featuredMedia.mediaDetails.width"
+      :alt="$page.wordPressPage.featuredMedia.altText"
+    />
+    <div v-html="$page.wordPressPage.content"/>
+  </Layout>
+</template>
+
+<page-query>
+query WordPressPage ($id: ID!) {
+  wordPressPage(id: $id) {
+    title
+    content
+    featuredMedia {
+      sourceUrl
+      altText
+      mediaDetails {
+        width
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo () {
+    return {
+      title: this.$page.wordPressPage.title
+    }
+  }
+}
+</script>
+
+<style>
+  ul.list {
+    list-style: none;
+    padding: 0;
+  }
+  ul.list li {
+    display: inline-block;
+    margin-right: 0.25em;
+  }
+  ul.list.tags li a {
+    padding: 0.25em 0.5em;
+    background-color: lightgray;
+  }
+  ul.list.categories li:after {
+    content: ',';
+    display: inline-block;
+  }
+  ul.list li:last-child:after {
+    content: '';
+  }
+</style>
