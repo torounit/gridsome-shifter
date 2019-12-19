2eb7022a921d7f740d66bd0747994435e9ee22e8
Hiroshi Urabe <mail@torounit.com>
2019-12-20 02:18:36 +0900

    fix wordpress page

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,7 +6,14 @@ module.exports = {
     WordPressCategory: '/category/:slug', // adds route for "category" post type (Optional)
     WordPressPost: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
     WordPressPostTag: '/tag/:slug', // adds route for "post_tag" post type (Optional)
-    WordPressPage: '/:slug' // adds route for "post_tag" post type (Optional)
+    WordPressPage: [
+      {
+        path: (node) => {
+          const url = new URL(node.link);
+          return `${url.pathname}`
+        }
+      }
+    ]
   },
 
   plugins: [
